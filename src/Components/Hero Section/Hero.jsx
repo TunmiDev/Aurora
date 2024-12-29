a8ffc769f7fec8fc60ee4f14fd5bec6c95c88e9a
TunmiDev <adewoleoluwatunmise@gmail.com>
2024-12-29 23:16:43 +0100

    updated

--- src/Components/Hero Section/Hero.jsx
+++ src/Components/Hero Section/Hero.jsx
@@ -4,19 +4,19 @@ import background from "../../assets/bg.png";
 const Hero = () => {
   return (
     <div>
-      <div className="h-screen flex items-center bg-gradient-to-r from-blue-500 to-blue-300">
+      <div className="h-screen flex items-center bg-gradient-to-r from-blue-500 to-blue-300 overflow-hidden">
         {/* Left Section (Text Content) */}
-        <div className="w-1/2 p-8 text-black dark:gray-950 dark:text-white duration-200">
-          <h1 className="text-6xl font-bold mt-[-50px] pt-[-50px]">
+        <div className="w-1/2 p-4 md:p-8 text-black dark:gray-950 dark:text-white duration-200">
+          <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold leading-tight">
             Welcome to Aurora's Kingdom!
           </h1>
-          <p className="mt-4 text-xl">
+          <p className="mt-4 text-sm md:text-lg lg:text-xl">
             Here, you'll discover the playful and curious world of my beloved
             bird, Aurora. From her daily adventures to her charming quirks,
             Aurora's life is a delightful journey filled with love, mischief,
             and endless cuddles.
           </p>
-          <button className="mt-6 px-4 py-2 flex items-center justify-center bg-gray-200 text-black rounded-full  hover:bg-gray-800 hover:text-white hover:underline dark:bg-gray-950 dark:text-white dark:hover:bg-white dark:hover:text-black duration-200">
+          <button className="mt-4 md:mt-6 px-4 md:px-6 py-2 md:py-3 bg-gray-200 text-black rounded-full hover:bg-gray-800 hover:text-white dark:bg-gray-950 dark:text-white dark:hover:bg-white dark:hover:text-black duration-200">
             View Gallery
           </button>
         </div>
