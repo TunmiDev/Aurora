5fa7d039decba64bb60bdfb036e0df9e71a723fb
TunmiDev <adewoleoluwatunmise@gmail.com>
2024-12-30 03:48:37 +0100

    updated blog

--- src/Components/Blog.jsx
+++ src/Components/Blog.jsx
@@ -1,8 +1,8 @@
 import React from "react";
-import photo1 from "../assets/photo1.jpg";
-import photo2 from "../assets/photo2.jpg";
-import photo3 from "../assets/photo3.jpg";
-import photo4 from "../assets/photo4.jpg";
+import vid1 from "../assets/vid1.mp4";
+import vid2 from "../assets/vid2.mp4";
+import vid3 from "../assets/vid3.mp4";
+import vid4 from "../assets/vid4.mp4";
 
 const AuroraPhotos = () => {
   return (
@@ -10,7 +10,7 @@ const AuroraPhotos = () => {
       {/* Text at the Top Center */}
       <div>
         <h1 className="relative inset-x-0 top-10 text-4xl sm:text-3xl md:text-6xl font-bold text-black dark:text-white text-center">
-          Aurora's Photos
+          Aurora's Blog
         </h1>
         <p className="relative inset-x-0 top-10 text-sm pt-6 sm:pt-8 text-black dark:text-white text-center">
           Welcome to Aurora's blog, where we share her latest adventures,
@@ -21,26 +21,49 @@ const AuroraPhotos = () => {
       </div>
       {/* Images */}
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4  gap-6 mt-20 mx-auto max-w-screen-lg px-4">
-        <img
-          src={photo1}
-          alt="bird pic"
+        <video
+          width="100%"
+          controls
+          autoPlay
+          loop
           className="w-80 h-80 object-cover rounded-lg"
-        />
-        <img
-          src={photo2}
-          alt="bird pic"
+        >
+          <source src={vid1} type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+
+        <video
+          width="100%"
+          controls
+          autoPlay
+          loop
           className="w-80 h-80 object-cover rounded-lg"
-        />
-        <img
-          src={photo3}
-          alt="bird pic"
+        >
+          <source src={vid2} type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+
+        <video
+          width="100%"
+          controls
+          autoPlay
+          loop
           className="w-80 h-80 object-cover rounded-lg"
-        />
-        <img
-          src={photo4}
-          alt="bird pic"
+        >
+          <source src={vid3} type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+
+        <video
+          width="100%"
+          controls
+          autoPlay
+          loop
           className="w-80 h-80 object-cover rounded-lg"
-        />
+        >
+          <source src={vid4} type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
       </div>
     </div>
   );
